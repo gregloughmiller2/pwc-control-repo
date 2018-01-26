88ac3b114a19411f20417752a83f773eed2d459f
Greg Loughmiller <gloughmiller@yahoo.com>
2018-01-26 15:05:01 -0500

    Add Win-Defender to configure real-time scanning

--- site/example/manifests/greg_demo_win_dsc.pp
+++ site/example/manifests/greg_demo_win_dsc.pp
@@ -1,14 +1,10 @@
 class example::greg_demo_win_dsc {
 #
-dsc_windowsfeature { 'IIS':
-  dsc_ensure       => present,
-  dsc_name  => 'Web-Server',
-  }
+dsc_xmppreference { 'Demo-Defender':
+   dsc_name             => 'Demo-Defender',
+   dsc_scanscheduleday  => 'Everyday',
+   dsc_scanparameters   => 'QuickScan',
+   dsc_disablerealtimemonitoring  => 'False',
+   }
 
-dsc_registry {'registry_demo':
-  dsc_ensure 	=> present,
-  dsc_key	=> 'HKEY_LOCAL_MACHINE\SOFTWARE\DemoKey',
-  dsc_valuename	=> 'DSCPuppet',
-  dsc_valuedata	=> 'This is a test with DSc Puppet Module',
-  } 
 }
